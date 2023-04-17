0ba525d37283f457746be7584e8aef1a0bc9bf39
hkkang <hkkang@sk.com>
2023-04-17 17:32:29 +0900

    인코딩 처리 추가

--- was/src/main/java/com/jin/was/ResponseHeader.java
+++ was/src/main/java/com/jin/was/ResponseHeader.java
@@ -12,6 +12,12 @@ public record ResponseHeader(
     int contentLength,
     Date date,
     String server
+    /*
+        TODO : encoding 설정
+        response.setContentType("text/html; charset=utf-8");
+        response.setCharacterEncoding("utf-8");
+        request.setCharacterEncoding("utf-8");
+     */
 ) {
     private static final String SERVER = "JHTTP 2.0"; // TODO
 
